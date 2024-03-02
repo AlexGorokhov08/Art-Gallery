cd45e7d99cd1291cf340f7bb2d89504ee84d0d85
AlexGorokhov08 <Alexgorohov89@gmail.com>
2024-03-02 17:10:35 +0400

    fix

--- imagesData.js
+++ imagesData.js
@@ -1,231 +1,231 @@
 export const imagesData = {
   "portraits-pencil": [
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG20231226000539.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG20231226000539.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG20231226000539_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG20231226000539_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG20231226232418.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG20231226232418.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG20231226232418_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG20231226232418_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG20240110171344.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG20240110171344.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG20240110171344_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG20240110171344_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG20240121134750.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG20240121134750.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG20240121134750_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG20240121134750_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG_20231223_000553.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG_20231223_000553.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG_20231223_000553_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG_20231223_000553_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG_20240116_162050.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG_20240116_162050.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG_20240116_162050_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG_20240116_162050_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG_20240212_122505.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG_20240212_122505.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG_20240212_122505_lowSize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG_20240212_122505_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/pencil/IMG_20240301_104623.jpg",
+      fullSizeSrc: "./imgs/portraits/pencil/IMG_20240301_104623.jpg",
       lowQualitySrc:
-        "imgs/portraits/pencil/lowsize/IMG_20240301_104623_lowsize.jpg",
+        "./imgs/portraits/pencil/lowsize/IMG_20240301_104623_lowsize.jpg",
     },
   ],
   "portraits-watercolor": [
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20231208094629.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20231208094629.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20231208094629_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG20231208094629_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20231126005610.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20231126005610.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20231126005610_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG20231126005610_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20231129231759.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20231129231759.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20231129231759_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG20231129231759_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20231201092652.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20231201092652.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20231201092652_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG20231201092652_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20231204233358.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20231204233358.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20231204233358_lowSize.jpg",
-    },
-
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG_20231128_101912.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG_20231128_101912_lowSize.jpg",
-    },
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG_20231205_000049.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG_20231205_000049_lowSize.jpg",
-    },
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20231211000052.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20231211000052_lowSize.jpg",
-    },
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG_20231215_101019.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG_20231215_101019_lowSize.jpg",
-    },
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20240207103655.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20240207103655_lowSize.jpg",
-    },
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20240210100859.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20240210100859_lowSize.jpg",
-    },
-    {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20240215154311.jpg",
-      lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20240215154311_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG20231204233358_lowSize.jpg",
     },
 
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG20240220104651.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG_20231128_101912.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG20240220104651_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG_20231128_101912_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/portraits/watercolor/IMG_20240223_125822.jpg",
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG_20231205_000049.jpg",
       lowQualitySrc:
-        "imgs/portraits/watercolor/lowsize/IMG_20240223_125822_lowSize.jpg",
+        "./imgs/portraits/watercolor/lowsize/IMG_20231205_000049_lowSize.jpg",
+    },
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20231211000052.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG20231211000052_lowSize.jpg",
+    },
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG_20231215_101019.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG_20231215_101019_lowSize.jpg",
+    },
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20240207103655.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG20240207103655_lowSize.jpg",
+    },
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20240210100859.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG20240210100859_lowSize.jpg",
+    },
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20240215154311.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG20240215154311_lowSize.jpg",
+    },
+
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG20240220104651.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG20240220104651_lowSize.jpg",
+    },
+    {
+      fullSizeSrc: "./imgs/portraits/watercolor/IMG_20240223_125822.jpg",
+      lowQualitySrc:
+        "./imgs/portraits/watercolor/lowsize/IMG_20240223_125822_lowSize.jpg",
     },
   ],
   "other-pencil": [
     {
-      fullSizeSrc: "imgs/other/pencil/IMG20230910194811.jpg",
+      fullSizeSrc: "./imgs/other/pencil/IMG20230910194811.jpg",
       lowQualitySrc:
-        "imgs/other/pencil/lowsize/IMG20230910194811_lowSize.jpg",
+        "./imgs/other/pencil/lowsize/IMG20230910194811_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/pencil/IMG20231019143756.jpg",
+      fullSizeSrc: "./imgs/other/pencil/IMG20231019143756.jpg",
       lowQualitySrc:
-        "imgs/other/pencil/lowsize/IMG20231019143756_lowSize.jpg",
+        "./imgs/other/pencil/lowsize/IMG20231019143756_lowSize.jpg",
     },
   ],
   "other-watercolor": [
     {
-      fullSizeSrc: "imgs/other/watercolor/IMG20230830121305.jpg",
+      fullSizeSrc: "./imgs/other/watercolor/IMG20230830121305.jpg",
       lowQualitySrc:
-        "imgs/other/watercolor/lowsize/IMG20230830121305_lowSize.jpg",
+        "./imgs/other/watercolor/lowsize/IMG20230830121305_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/watercolor/IMG20230907124746.jpg",
+      fullSizeSrc: "./imgs/other/watercolor/IMG20230907124746.jpg",
       lowQualitySrc:
-        "imgs/other/watercolor/lowsize/IMG20230907124746_lowSize.jpg",
+        "./imgs/other/watercolor/lowsize/IMG20230907124746_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/watercolor/IMG20231108233403.jpg",
+      fullSizeSrc: "./imgs/other/watercolor/IMG20231108233403.jpg",
       lowQualitySrc:
-        "imgs/other/watercolor/lowsize/IMG20231108233403_lowSize.jpg",
+        "./imgs/other/watercolor/lowsize/IMG20231108233403_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/watercolor/IMG_20231110_000930.jpg",
+      fullSizeSrc: "./imgs/other/watercolor/IMG_20231110_000930.jpg",
       lowQualitySrc:
-        "imgs/other/watercolor/lowsize/IMG_20231110_000930_lowSize.jpg",
+        "./imgs/other/watercolor/lowsize/IMG_20231110_000930_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/watercolor/IMG_20231112_095424.jpg",
+      fullSizeSrc: "./imgs/other/watercolor/IMG_20231112_095424.jpg",
       lowQualitySrc:
-        "imgs/other/watercolor/lowsize/IMG_20231112_095424_lowSize.jpg",
+        "./imgs/other/watercolor/lowsize/IMG_20231112_095424_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/watercolor/IMG_20231113_234801.jpg",
+      fullSizeSrc: "./imgs/other/watercolor/IMG_20231113_234801.jpg",
       lowQualitySrc:
-        "imgs/other/watercolor/lowsize/IMG_20231113_234801_lowSize.jpg",
+        "./imgs/other/watercolor/lowsize/IMG_20231113_234801_lowSize.jpg",
     },
   ],
   "other-gouache": [
     {
-      fullSizeSrc: "imgs/other/gouache/IMG20230715181806.jpg",
+      fullSizeSrc: "./imgs/other/gouache/IMG20230715181806.jpg",
       lowQualitySrc:
-        "imgs/other/gouache/lowsize/IMG20230715181806_lowSize.jpg",
+        "./imgs/other/gouache/lowsize/IMG20230715181806_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/gouache/IMG20230812124424.jpg",
+      fullSizeSrc: "./imgs/other/gouache/IMG20230812124424.jpg",
       lowQualitySrc:
-        "imgs/other/gouache/lowsize/IMG20230812124424_lowSize.jpg",
+        "./imgs/other/gouache/lowsize/IMG20230812124424_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/gouache/IMG20230816141141.jpg",
+      fullSizeSrc: "./imgs/other/gouache/IMG20230816141141.jpg",
       lowQualitySrc:
-        "imgs/other/gouache/lowsize/IMG20230816141141_lowSize.jpg",
+        "./imgs/other/gouache/lowsize/IMG20230816141141_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/gouache/IMG20230831165409.jpg",
+      fullSizeSrc: "./imgs/other/gouache/IMG20230831165409.jpg",
       lowQualitySrc:
-        "imgs/other/gouache/lowsize/IMG20230831165409_lowSize.jpg",
+        "./imgs/other/gouache/lowsize/IMG20230831165409_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/gouache/IMG_20231119_000500.jpg",
+      fullSizeSrc: "./imgs/other/gouache/IMG_20231119_000500.jpg",
       lowQualitySrc:
-        "imgs/other/gouache/lowsize/IMG_20231119_000500_lowSize.jpg",
+        "./imgs/other/gouache/lowsize/IMG_20231119_000500_lowSize.jpg",
     },
   ],
   "other-graphic": [
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230720144006.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230720144006.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230720144006_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230720144006_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230720153926.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230720153926.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230720153926_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230720153926_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230723161552.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230723161552.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230723161552_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230723161552_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230730223706.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230730223706.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230730223706_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230730223706_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230807220612.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230807220612.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230807220612_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230807220612_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230827105850.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230827105850.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230827105850_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230827105850_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG20230919145612.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG20230919145612.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG20230919145612_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG20230919145612_lowSize.jpg",
     },
     {
-      fullSizeSrc: "imgs/other/graphic/IMG_20230824_171125.jpg",
+      fullSizeSrc: "./imgs/other/graphic/IMG_20230824_171125.jpg",
       lowQualitySrc:
-        "imgs/other/graphic/lowsize/IMG_20230824_171125_lowSize.jpg",
+        "./imgs/other/graphic/lowsize/IMG_20230824_171125_lowSize.jpg",
     },
   ],
 };
